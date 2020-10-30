1d660db8889cf521f4deef80d6a1c2c49f451889
Felix Febrian <f.febrian96@gmail.com>
2020-10-30 16:28:01 +0100

    Status on 30.10.2020

--- yfinance/base.py
+++ yfinance/base.py
@@ -280,16 +280,19 @@ class TickerBase():
         data = utils.get_json(url, proxy)
 
         # holders
-        url = "{}/{}/holders".format(self._scrape_url, self.ticker)
-        holders = _pd.read_html(url)
-        self._major_holders = holders[0]
-        self._institutional_holders = holders[1]
-        if 'Date Reported' in self._institutional_holders:
-            self._institutional_holders['Date Reported'] = _pd.to_datetime(
-                self._institutional_holders['Date Reported'])
-        if '% Out' in self._institutional_holders:
-            self._institutional_holders['% Out'] = self._institutional_holders[
-                '% Out'].str.replace('%', '').astype(float)/100
+        url = "{}/{}".format(self._scrape_url, self.ticker)
+        try: # TODO: workaround for german stocks - holders not returning
+            holders = _pd.read_html(url + "/holders")
+            self._major_holders = holders[0]
+            self._institutional_holders = holders[1]
+            if 'Date Reported' in self._institutional_holders:
+                self._institutional_holders['Date Reported'] = _pd.to_datetime(
+                    self._institutional_holders['Date Reported'])
+            if '% Out' in self._institutional_holders:
+                self._institutional_holders['% Out'] = self._institutional_holders[
+                    '% Out'].str.replace('%', '').astype(float)/100
+        except:
+            pass
 
         # sustainability
         d = {}
